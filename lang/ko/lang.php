2aa32d538d43adbea39828ea4f3fce6afe4064e5
SC Yoo <dryoo@live.com>
2014-10-02 22:30:43 +0900

    korean language

---
+++ lang/ko/lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * English language file for imgpaste plugin
+ *
+ * @author Andreas Gohr <gohr@cosmocode.de>
+ */
+
+$lang['e_nodata']         = '클립보드에 사진이 없습니다.';
+$lang['js']['inprogress'] = '올리는 중입니다. 기다려주세요.';
+
+
+
+//Setup VIM: ex: et ts=4 :
