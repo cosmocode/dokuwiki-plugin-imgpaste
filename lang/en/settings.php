293e8e8db04ae8fd41b134e3ffd69e797b1af57a
fstorck <github@florianstorck.de>
2018-05-03 15:21:32 +0200

    CHG: - updated settings info

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -6,7 +6,7 @@
  */
 
 // keys need to match the config setting name
-$lang['filename'] = 'How to name pasted images. You can use the placeholders @USER@, @NS@, @ID@ and strftime placeholders. The file extension is added automatically.';
+$lang['filename'] = 'How to name pasted images. You can use the placeholders @USER@, @NS@, @ID@, @PAGE@ and strftime placeholders. The file extension is added automatically.';
 
 
 
