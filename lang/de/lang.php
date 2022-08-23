44af8ccbc143a0ccbc1907385e5dc7a604137b6e
Michael Fischer <dokuwiki@wissensallmen.de>
2022-08-23 09:39:09 +0200

    German translation

---
+++ lang/de/lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * German language file for imgpaste plugin
+ *
+ * @author Michael Fischer <dokuwiki@m-fischer.info>
+ */
+
+$lang['e_nodata']         = 'Die Zwischenablage enthält keine Daten.';
+$lang['js']['inprogress'] = 'Upload läuft… bitte warten.';
+
---
+++ lang/de/settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * German language file for imgpaste plugin
+ *
+ * @author Michael Fischer <dokuwiki@m-fischer.info>
+ */
+
+// keys need to match the config setting name
+$lang['filename'] = 'So werden eingefügte Bilder benannt. Sie können die Platzhalter @USER@, @NS@, @ID@, @PAGE@ und die Platzhalter der PHP-Funktion strftime verwenden. Die Dateierweiterung wird automatisch hinzugefügt.';
+
+
